3118f5ab12965c7686ce6e8a45c0b9cb6589440c
3squ1vo <lcarlosdossantos066@outlook.com>
2026-03-19 17:49:08 -0300

    Esse arquivo é para a Pandora funcionar perfeitamente

---
+++ config_pandora.py
@@ -0,0 +1,16 @@
+# ------------ Chaves das APIs ------------
+GROQ_API_KEY = "sua_chave_api_do_groq_aqui"
+GEMINI_API_KEY = "sua_chave_api_do_gemini_aqui"
+OPENWEATHERMAP_API_KEY = "sua_chave_api_do_openweathermap_aqui"
+
+#------------ Modelos e parte basica da Panddora ------------
+MODELO_GROQ = "llama-3.3-70b-versatile"
+MODELO_GEMINI = "gemini-1.5-flash"
+#INDICE_MICROFONE = 1
+
+'''
+* O INDICE_MICROFONE vai de você se quiser ativar, no meu SO não precisei
+
+* Os modelos que estão ai, podem ser alterados (mas, use somente os modelos do Groq e Gemini) e caso queira
+alterar ou mudar os modelos, se lembre de mudar as variáveis e os trechos do código corespondentes a eles.
+'''
